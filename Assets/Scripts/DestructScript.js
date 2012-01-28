17f71c94cd748ed0153abd39dce9767990401062
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2012-01-28 11:52:46 -0500

    added town detection

--- Assets/Scripts/DestructScript.js
+++ Assets/Scripts/DestructScript.js
@@ -5,9 +5,13 @@ function Start () {}
 function Update () {}
 
 function OnCollisionEnter(collision : Collision) {
-	if (collision.collider.tag.IndexOf("box") != -1) {
-		// ...
-
+	if (collision.collider.tag.IndexOf("towna") != -1) {
+		Debug.Log("Arrived at Town A!");
+	}
+	else if (collision.collider.tag.IndexOf("townb") != -1) {
+		Debug.Log("Arrived at Town B!");
+	}
+	else if (collision.collider.tag.IndexOf("box") != -1) {
 		Debug.Log("Box Collision!");
 	}
 }
